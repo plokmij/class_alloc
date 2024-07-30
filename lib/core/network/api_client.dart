55dbca2a8b12d01d006f9df2a4409e62195ccd6f
Samfan <samfan@live.com>
2024-07-31 03:41:19 +0530

    feat: add http methods to api client

--- lib/core/network/api_client.dart
+++ lib/core/network/api_client.dart
@@ -9,4 +9,24 @@ class ApiClient {
     );
     _dio.options = options;
   }
+
+  Future<Response> get(String path) async {
+    return await _dio.get(path);
+  }
+
+  Future<Response> post(String path, dynamic data) async {
+    return await _dio.post(path, data: data);
+  }
+
+  Future<Response> put(String path, dynamic data) async {
+    return await _dio.put(path, data: data);
+  }
+
+  Future<Response> delete(String path) async {
+    return await _dio.delete(path);
+  }
+
+  Future<Response> patch(String path, dynamic data) async {
+    return await _dio.patch(path, data: data);
+  }
 }
