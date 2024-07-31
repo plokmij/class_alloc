d2d1a9181899ab43039ad586cac70a447d9aa43c
Samfan <samfan@live.com>
2024-07-31 10:14:20 +0530

    refactor: Update ClassroomDetail constructor

--- lib/features/classrooms/domain/entities/classroom_detail.dart
+++ lib/features/classrooms/domain/entities/classroom_detail.dart
@@ -4,10 +4,12 @@ class ClassroomDetail extends Classroom {
   final String subject;
 
   ClassroomDetail({
-    required int id,
-    required String name,
-    required int size,
-    required Layout layout,
+    required super.id,
+    required super.name,
+    required super.size,
+    required super.layout,
     required this.subject,
-  }) : super(id: id, name: name, size: size, layout: layout);
+  });
+
+  bool get hasSubject => subject.isNotEmpty;
 }
