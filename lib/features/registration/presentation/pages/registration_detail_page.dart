11ea2532706e947b782717a929896ee0a891cdb2
Samfan <samfan@live.com>
2024-08-01 06:52:30 +0530

    ui: app text style

--- lib/features/registration/presentation/pages/registration_detail_page.dart
+++ lib/features/registration/presentation/pages/registration_detail_page.dart
@@ -29,107 +29,116 @@ class _RegistrationDetailPageState extends State<RegistrationDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Registration Detail'),
+        title: const Text(
+          'Registration',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: BlocConsumer<RegistrationDetailCubit, RegistrationDetailState>(
-        listener: (context, state) {
-          if (state.errorMessage != null) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                backgroundColor: Colors.red,
-                content: Text(state.errorMessage!),
-                behavior: SnackBarBehavior.floating,
-              ),
-            );
-          } else if (state.deleteSuccess) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Registration Deleted Successfully'),
-                behavior: SnackBarBehavior.floating,
-              ),
-            );
-            context.pop();
-          }
-        },
-        builder: (context, state) {
-          if (state.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state.registration != null) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  ItemTile(
-                    title: 'Student Details',
-                    subtitle: state.registration!.student.name +
-                        '\n' +
-                        state.registration!.student.email,
-                    trailing: Text('Age: ${state.registration!.student.age}'),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ItemTile(
-                    title: 'Subject Details',
-                    subtitle: state.registration!.subject.name +
-                        '\n' +
-                        state.registration!.subject.teacher,
-                    trailing:
-                        Text('Credit: ${state.registration!.subject.credits}'),
-                  ),
-                  Spacer(),
-                  CupertinoButton(
-                    color: Colors.red,
-                    onPressed: () {
-                      showCupertinoDialog(
-                        context: context,
-                        builder: (context) => CupertinoAlertDialog(
-                          title: Text('Delete'),
-                          content: Text(
-                            'Do you want to delete?',
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocConsumer<RegistrationDetailCubit, RegistrationDetailState>(
+          listener: (context, state) {
+            if (state.errorMessage != null) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: Colors.red,
+                  content: Text(state.errorMessage!),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            } else if (state.deleteSuccess) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Registration Deleted Successfully'),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+              context.pop();
+            }
+          },
+          builder: (context, state) {
+            if (state.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state.registration != null) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ItemTile(
+                      title: 'Student Details',
+                      subtitle: state.registration!.student.name +
+                          '\n' +
+                          state.registration!.student.email,
+                      trailing: Text('Age: ${state.registration!.student.age}'),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    ItemTile(
+                      title: 'Subject Details',
+                      subtitle: state.registration!.subject.name +
+                          '\n' +
+                          state.registration!.subject.teacher,
+                      trailing: Text(
+                          'Credit: ${state.registration!.subject.credits}'),
+                    ),
+                    Spacer(),
+                    CupertinoButton(
+                      color: Colors.red,
+                      onPressed: () {
+                        showCupertinoDialog(
+                          context: context,
+                          builder: (context) => CupertinoAlertDialog(
+                            title: Text('Delete'),
+                            content: Text(
+                              'Do you want to delete?',
+                            ),
+                            actions: [
+                              CupertinoDialogAction(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              CupertinoDialogAction(
+                                onPressed: () {
+                                  context.pop();
+                                  context
+                                      .read<RegistrationDetailCubit>()
+                                      .deleteRegistration(
+                                          widget.registrationId);
+                                },
+                                child: Text('Delete'),
+                              ),
+                            ],
                           ),
-                          actions: [
-                            CupertinoDialogAction(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('Cancel'),
-                            ),
-                            CupertinoDialogAction(
-                              onPressed: () {
-                                context.pop();
-                                context
-                                    .read<RegistrationDetailCubit>()
-                                    .deleteRegistration(widget.registrationId);
-                              },
-                              child: Text('Delete'),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    child: Text('Delete Registration'),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                ],
-              ),
-            );
-          } else if (state.errorMessage != null) {
-            return ErrorRetryWidget(
-              onRetryPressed: () {
-                context
-                    .read<RegistrationDetailCubit>()
-                    .loadRegistration(widget.registrationId);
-              },
-              errorMessage: state.errorMessage!,
-            );
-          }
-          return const SizedBox.shrink();
-        },
+                        );
+                      },
+                      child: Text('Delete Registration'),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ],
+                ),
+              );
+            } else if (state.errorMessage != null) {
+              return ErrorRetryWidget(
+                onRetryPressed: () {
+                  context
+                      .read<RegistrationDetailCubit>()
+                      .loadRegistration(widget.registrationId);
+                },
+                errorMessage: state.errorMessage!,
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
--- lib/features/registration/presentation/pages/registrations_page.dart
+++ lib/features/registration/presentation/pages/registrations_page.dart
@@ -2,6 +2,7 @@ import 'package:class_alloc/core/presentation/widgets/error_retry_widget.dart';
 import 'package:class_alloc/features/registration/presentation/blocs/registration_cubit/registration_cubit.dart';
 import 'package:class_alloc/features/registration/presentation/widgets/registrations_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
@@ -23,67 +24,76 @@ class _RegistrationsPageState extends State<RegistrationsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Registration'),
+        title: const Text(
+          'Registration',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            flex: 8,
-            child: BlocBuilder<RegistrationCubit, RegistrationState>(
-              builder: (context, state) {
-                if (state is RegistrationInitial ||
-                    state is RegistrationLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state is RegistrationLoaded) {
-                  if (state.registrations.isEmpty) {
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 8,
+              child: BlocBuilder<RegistrationCubit, RegistrationState>(
+                builder: (context, state) {
+                  if (state is RegistrationInitial ||
+                      state is RegistrationLoading) {
                     return const Center(
-                      child: Text(
-                        'No data',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
+                      child: CircularProgressIndicator(),
                     );
-                  } else {
-                    return RegistrationsList(
-                        registrations: state.registrations);
+                  } else if (state is RegistrationLoaded) {
+                    if (state.registrations.isEmpty) {
+                      return const Center(
+                        child: Text(
+                          'No data',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return RegistrationsList(
+                          registrations: state.registrations);
+                    }
+                  } else if (state is RegistrationError) {
+                    return ErrorRetryWidget(
+                      onRetryPressed: () {
+                        context.read<RegistrationCubit>().loadRegistrations();
+                      },
+                      errorMessage: state.message,
+                    );
                   }
-                } else if (state is RegistrationError) {
-                  return ErrorRetryWidget(
-                    onRetryPressed: () {
-                      context.read<RegistrationCubit>().loadRegistrations();
-                    },
-                    errorMessage: state.message,
-                  );
-                }
-                return const SizedBox.shrink();
-              },
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Center(
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(9),
-                  ),
-                  backgroundColor: Colors.blue.shade100,
-                ),
-                onPressed: () {
-                  context.push('/registration/add');
+                  return const SizedBox.shrink();
                 },
-                child: Text(
-                  'New Registration',
-                  style: TextStyle(
-                      color: Colors.blue.shade600, fontWeight: FontWeight.bold),
-                ),
               ),
             ),
-          )
-        ],
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    backgroundColor: Colors.blue.shade100,
+                  ),
+                  onPressed: () {
+                    context.push('/registration/add');
+                  },
+                  child: Text(
+                    'New Registration',
+                    style: TextStyle(
+                        color: Colors.blue.shade600,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
